01e5bf010e6ab4497d65980521d29323fa2ec188
Akshay N V <akshayvenkatesha@gmail.com>
2019-02-13 20:42:15 +0530

    added code for PCI compliance.

---
+++ scripts/pci/mini/disable_http_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+is_pkg_installed()
+{
+    PKG_NAME=$1
+    if $(dpkg -s $PKG_NAME 2> /dev/null | grep -q '^Status: install ') ; then
+        # debug "$PKG_NAME is installed"
+        FNRET=0
+    else
+        # debug "$PKG_NAME is not installed"
+        FNRET=1
+    fi
+}
+
+PACKAGES='nginx apache2 lighttpd micro-httpd mini-httpd yaws boa bozohttpd'
+
+failed=0
+for PACKAGE in $PACKAGES; do
+    is_pkg_installed $PACKAGE
+    if [ $FNRET = 0 ]; then
+        failed=1
+    fi
+done
+
+if [ $failed = 1 ]; then
+    echo "Failed"
+else
+    echo "Passed"
+fi
