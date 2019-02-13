61fcd85018a7638d453e19be49f9a2c3241651a6
Akshay N V <akshayvenkatesha@gmail.com>
2019-02-13 21:37:46 +0530

    fixing disable_cramfs.sh

--- scripts/cis/mini/disable_cramfs.sh
+++ scripts/cis/mini/disable_cramfs.sh
@@ -3,7 +3,7 @@
 #echo - e "\e[92m== 1.1.1.1 Ensure mounting of cramfs filesystems is disabled ==\e\n"
 
 disable_cramfs () {	
-	if [[ "$(modprobe -n -v cramfs 2>/dev/null)" =  * install * ]]
+	if [[ "$(modprobe -n -v cramfs 2>/dev/null)" =  *install* ]]
 		then echo "Passed"
 	else
 		echo "Failed"
